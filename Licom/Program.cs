9491c1dfeb33c8b28486ee9b5fd28071ccf520c3
AartiPundalik <aartipundalik68@gmail.com>
2022-12-20 00:51:52 +0530

    linecomp

---
+++ Licom/Program.cs
@@ -0,0 +1,20 @@
+﻿using Licom;
+
+namespace  LineComparison
+{
+    public class  licom
+    {
+        public static void Main(String[] args)
+        {
+            length.LineComparison();
+            endpoint.checklines();
+        }
+
+    }
+}
+
+        
+
+    
+
+
---
+++ Licom/length.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Licom
+{
+    public class length
+    {
+        public static void LineComparison()
+        {
+            Console.WriteLine("Enter the first coordinates (x1 & y1) : ");
+            double x1 = Convert.ToInt32(Console.ReadLine());
+            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second coordinates (x2 & y2) : ");
+            double x2 = Convert.ToInt32(Console.ReadLine());
+            double y2 = Convert.ToInt32(Console.ReadLine());
+
+            double temp1 = Math.Pow((x2 - x1), 2);
+            double temp2 = Math.Pow((y2 - y1), 2);
+            double result = Math.Sqrt(temp1 + temp2);
+
+            Console.WriteLine(result);
+
+
+        }
+
+    }
+}
+
+ 
+
+    
+
